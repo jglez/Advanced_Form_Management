d0bc502e5d1a9e8df109392f71e6c8268857f5f7
Jorge Gonzalez <jglez.cs@gmail.com>
2020-12-17 17:43:11 -0600

    Added state to Form.js, set an array of objs into state

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+// Default values for the form
 const initialFormValues = {
   name: '',
   email: '',
@@ -7,10 +8,30 @@ const initialFormValues = {
   ToS: false
 }
 
+// Simulated database
+const usersList = [
+  { name: 'Elon Musk', email: 'starman@spacex.com', password: 'occupy mars' },
+  { name: 'George Hotz', email: 'geohot@commaai.com', password: 'cya_tesla' },
+  { name: 'John Carmack', email: 'jcarmack@id.com', password: 'doom1234' }
+]
+
 export default function Form() {
+  const [users, setUsers] = useState(usersList)
+  const [formValues, setFormValues] = useState(initialFormValues)
 
   return (
     <>
+      <h2>Members:</h2>
+
+      {users.map((user, index) => {
+        return (
+          <div key={index}>
+            <b>Name:</b> {user.name} - <b>Email:</b> {user.email}
+          </div>
+        )
+
+      })}
+
       <form>
         <label>Name&nbsp;
           <input
